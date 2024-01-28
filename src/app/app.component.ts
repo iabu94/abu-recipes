ca6cd0bdc45ce6c8211782fca6f0fe8989858fcd
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2024-01-28 11:47:04 +0530

    Basic Menu Setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { MatButton, MatIconButton } from '@angular/material/button';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { MatList, MatListItem } from '@angular/material/list';
+import {
+  MatDrawer,
+  MatDrawerContainer,
+  MatDrawerContent,
+} from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatToolbar,
+    MatIcon,
+    MatButton,
+    MatIconButton,
+    MatDrawerContainer,
+    MatDrawer,
+    MatDrawerContent,
+    MatList,
+    MatListItem,
+    MatDivider,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'abu-recipes';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'all', pathMatch: 'full' },
+  {
+    path: 'all',
+    loadComponent: () =>
+      import('./recipe-list/recipe-list.component').then(
+        (c) => c.RecipeListComponent
+      ),
+  },
+];
