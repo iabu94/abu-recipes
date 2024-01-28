4fb2185d343c00ce098ac19e68c11064ab13ba1e
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2024-01-28 12:16:52 +0530

    Added Search Component

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { SearchFieldComponent } from '../search-field/search-field.component';
 
 @Component({
   selector: 'app-recipe-list',
   standalone: true,
-  imports: [],
+  imports: [SearchFieldComponent],
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.scss'
+  styleUrl: './recipe-list.component.scss',
 })
 export class RecipeListComponent {
-
+  searchKey = signal<string>('');
 }
---
+++ src/app/search-field/search-field.component.ts
@@ -0,0 +1,41 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  FormControl,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatIcon } from '@angular/material/icon';
+import { MatInput } from '@angular/material/input';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
+
+@Component({
+  selector: 'app-search-field',
+  standalone: true,
+  imports: [
+    MatFormField,
+    MatLabel,
+    MatIcon,
+    FormsModule,
+    MatInput,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './search-field.component.html',
+  styleUrl: './search-field.component.scss',
+})
+export class SearchFieldComponent {
+  formControl = new FormControl('', [Validators.required]);
+
+  @Output() searchKey = new EventEmitter<string>();
+
+  constructor() {
+    this.formControl.valueChanges
+      .pipe(distinctUntilChanged(), debounceTime(500))
+      .subscribe((value) => {
+        if (value) {
+          this.searchKey.emit(value);
+        }
+      });
+  }
+}
